a94daa2af74b053ceb1bbfbf528a9677a3af0d66
yash <yashwanthvarmaalluri007@gmail.com>
2024-10-19 17:49:25 -0400

    SAMPLE

---
+++ sample.java
@@ -0,0 +1,30 @@
+// Your First Program
+
+class Computer{
+    private int n = 5;
+    
+    public int getN(){
+        return n;
+    }
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void playMusic(){
+        System.out.println("Playing Music");
+    }
+
+    public String getMeAPen(int num){
+        return "Hell";
+    }
+}
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Computer c = new Computer();
+        c.playMusic();
+        System.out.println(c.getMeAPen(0));
+        c.setN(20);
+        System.out.println(c.getN());
+    }
+}
